56d784b9417c3cdfc1483d245f586f226826be46
Oliver Lee <oliverzlee@gmail.com>
2025-06-11 00:02:59 -0700

    enable clangd and LSP settings for Zed
    Add a target to generate a `compile_commands.json` file which can be
    used with clangd. This file can be generated with
    
    ```
    bazel run //tools:compile_commands
    ```
    
    and will be generated in the root directory.
    
    This file needs to be periodically regenerated, for example, when a new
    translation unit is defined.
    
    This commit also defines `.clangd` and `.zed` settings. `.clangd`
    defines additional compilation options necessary for use of `clangd`
    with the hermetic clang toolchain.
    
    Change-Id: I02ae78c1110a7012611fcaa400adcfc592839c0f

--- sel/test/dummy_test.cpp
+++ sel/test/dummy_test.cpp
@@ -1,9 +1,11 @@
+#include "sel/sel.hpp"
 #include "skytest/skytest.hpp"
 
 auto main() -> int
 {
   using namespace skytest::literals;
+  using ::skytest::eq;
   using ::skytest::expect;
 
-  "dummy test"_ctest = [] { return expect(true); };
+  "dummy test"_test = [] { return expect(eq(42, sel::foo())); };
 }
