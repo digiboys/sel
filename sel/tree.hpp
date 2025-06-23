f60a195f662fd5c23c12d23774ff59fbff048f36
Oliver Lee <oliverzlee@gmail.com>
2025-06-22 18:32:44 -0700

    construct `tree` from elements
    Add additional constructors to `tree` to construct it from elements
    instead of a `leaf` or `operation`. This allows construction of a `tree`
    from sub-`tree`s.
    
    Additionally, define tag types to denote `plus` and `multiplies`. These
    tags denote the operation, but do not store operands.
    
    Change-Id: Ideb8ce4bfe9d2c52a71355d74ba5cea57cbad579

--- sel/tree.hpp
+++ sel/tree.hpp
@@ -1,17 +1,23 @@
 #pragma once
 
 #include "sel/constant.hpp"
+#include "sel/contract.hpp"
 #include "sel/detail/tuple_ref.hpp"
 #include "sel/detail/tuple_transform_reduce.hpp"
 #include "sel/leaf.hpp"
 #include "sel/multiplies.hpp"
 #include "sel/operation.hpp"
 #include "sel/plus.hpp"
+#include "sel/term.hpp"
 #include "sel/variable.hpp"
 
+#include <concepts>
 #include <cstddef>
 #include <functional>
+#include <ranges>
 #include <tuple>
+#include <type_traits>
+#include <utility>
 #include <variant>
 #include <vector>
 
@@ -21,43 +27,83 @@ namespace sel {
 class tree
 {
   /// internal (non-terminal) element describing an operation
-  template <template <class...> class op>
-  struct node
+  template <class OpTag>
+  struct op_node
   {
-    template <class... Args>
-    using operation_type = op<Args...>;
-
     std::vector<tree> args;
 
     template <class... Args>
-    constexpr explicit node(const Args&... args)
-        : args{tree{args}...}
+      requires (
+          (std::same_as<tree, std::remove_cvref_t<Args>> or
+           term<std::remove_cvref_t<Args>>) and
+          ...
+      )
+    constexpr explicit op_node(Args&&... args)
+        : args{tree{std::forward<Args>(args)}...}
     {}
 
-    friend auto operator==(const node&, const node&) -> bool = default;
+    constexpr op_node(std::from_range_t, std::ranges::sized_range auto& values)
+    {
+      SEL_PRECONDITION(std::ranges::size(values) != 0);
+      args.reserve(std::ranges::size(values));
+
+      for (auto&& value : values) {
+        args.emplace_back(std::forward<decltype(value)>(value));
+      }
+    }
+
+    friend auto operator==(const op_node&, const op_node&) -> bool = default;
   };
 
   using node_type = std::variant<
       constant<int>,
       constant<double>,
       variable,
-      node<plus>,
-      node<multiplies>>;
+      op_node<op::plus>,
+      op_node<op::multiplies>>;
 
   node_type value_;
 
 public:
-  /// construct a `tree` from a `leaf`
-  constexpr explicit tree(const leaf auto& value)
-      : value_{value}
+  /// construct from a `leaf`
+  template <class T>
+    requires leaf<std::remove_cvref_t<T>>
+  constexpr explicit tree(T&& value)
+      : value_{std::forward<T>(value)}
   {}
 
-  /// construct a `tree` from an `operation`
-  template <template <class...> class op, class... Args>
-    requires operation<op<Args...>>
-  constexpr explicit tree(const op<Args...>& expr)
-      : value_{std::make_from_tuple<node<op>>(expr.args)}
+  /// construct from an `operation`
+  template <class Op>
+    requires (not std::same_as<tree, std::remove_cvref_t<Op>>) and
+             operation<std::remove_cvref_t<Op>>
+  constexpr explicit tree(Op&& expr)
+      : value_{std::make_from_tuple<op_node<typename std::remove_cvref_t<
+            Op>::op_tag>>(std::forward<Op>(expr).args)}
   {}
+
+  /// construct from elements
+  /// @{
+  template <leaf L, class T>
+    requires std::constructible_from<L, T>
+  constexpr tree(std::in_place_type_t<L>, T&& value)
+      : value_{std::in_place_type<L>, std::forward<T>(value)}
+  {}
+
+  template <class OpTag, class... Args>
+    requires (sizeof...(Args) != 0)
+  constexpr tree(std::in_place_type_t<OpTag>, Args&&... args)
+      : value_{std::in_place_type<op_node<OpTag>>, std::forward<Args>(args)...}
+  {}
+
+  template <class OpTag>
+  constexpr tree(
+      std::in_place_type_t<OpTag>,
+      std::from_range_t,
+      std::ranges::sized_range auto&& args
+  )
+      : value_{std::in_place_type<op_node<OpTag>>, std::from_range, args}
+  {}
+  /// @}
 
   /// compare a `tree` with a `leaf`
   [[nodiscard]]
@@ -78,7 +124,7 @@ public:
   [[nodiscard]]
   constexpr auto operator==(const op<Args...>& other) const -> bool
   {
-    if (const auto value = std::get_if<node<op>>(&value_)) {
+    if (const auto value = std::get_if<op_node<operation_tag<op>>>(&value_)) {
       if (value->args.size() == sizeof...(Args)) {
         return detail::tuple_transform_reduce(
             detail::tuple_ref<sizeof...(Args)>(value->args),
